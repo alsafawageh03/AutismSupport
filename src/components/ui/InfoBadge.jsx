ac367d242f2e9b44060039130a3599e651992ed1
alsafawageh03 <shwageh55@gmail.com>
2026-07-01 21:32:22 +0300

    chore: finalize remaining changes

--- src/components/ui/InfoBadge.jsx
+++
@@ -1,9 +0,0 @@
-function InfoBadge() {
-  return (
-    <div className="relative w-10 h-10 shrink-0 overflow-hidden rounded-full bg-[var(--primary-500)] flex items-center justify-center text-[var(--white)] text-lg font-bold">
-      i
-    </div>
-  );
-}
-
-export default InfoBadge;
